232c8828c421cd7e6156122f3ce12b8b90e4c9aa
Angelo Amadora <amadora.angelo@gmail.com>
2017-02-03 18:45:51 +0800

    Enemy Scripts and Player Scripts
    Player moves given user input
    Enemies move at a set pattern

--- GAM-DEV Space Invaders/Assets/EnemyScript.cs
+++ GAM-DEV Space Invaders/Assets/EnemyScript.cs
@@ -3,14 +3,64 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class EnemyScript : MonoBehaviour {
-
-	// Use this for initialization
-	void Start () {
-		
-	}
+    private Transform trans;
+    private int border1;
+    private bool down;
+    private int downFrames;
+    private bool direction;
+    // Use this for initialization
+    void Start () {
+        trans = transform;
+        border1 = 70;
+        down = false;
+        downFrames = 10;
+        direction = true;
+    }
 	
 	// Update is called once per frame
 	void Update () {
-		
+            
+        if(!down)
+        {
+            if(direction)
+            {
+                trans.Translate(0.1f * 0.5f, 0, 0);
+                border1--;
+                if (border1 == 0)
+                {
+                    down = true;
+                }
+            }
+            else
+            {
+                trans.Translate(-0.1f * 0.5f, 0, 0);
+                border1--;
+                if (border1 == 0)
+                {
+                    down = true;
+                }
+            }
+            
+
+                
+        }
+        else
+        {
+            trans.Translate(0, -0.1f * 0.5f, 0);
+            downFrames--;
+            if (downFrames == 0)
+            {
+                down = false;
+                downFrames = 10;
+                border1 = 70;
+                if (direction)
+                    direction = false;
+                else
+                    direction = true;
+            }
+        }
+            
+
+
 	}
 }
--- GAM-DEV Space Invaders/Assets/playerScript.cs
+++ GAM-DEV Space Invaders/Assets/playerScript.cs
@@ -3,14 +3,19 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class playerScript : MonoBehaviour {
-
+    private Transform trans;
 	// Use this for initialization
 	void Start () {
-		
+        trans = transform;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
+        float offsetX = Input.GetAxis("Horizontal");
+        if (offsetX > 0)
+            trans.Translate(offsetX*0.5f, 0, 0);
+        else if (offsetX < 0)
+            trans.Translate(offsetX*0.5f, 0, 0);
+
 	}
 }
