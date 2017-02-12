45b48679007b934ec34fb52e93343cb6ed931efa
miguelgomez1 <miguel_gomez01@yahoo.com>
2017-02-12 22:35:53 +0800

    Game Over Game Over added when player dies

--- GAM-DEV Space Invaders/Assets/EnemyScripts/enemyBullet.cs
+++ GAM-DEV Space Invaders/Assets/EnemyScripts/enemyBullet.cs
@@ -1,10 +1,12 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class enemyBullet : MonoBehaviour {
     public Transform trans;
     public GameObject bulletBody;
+    public GameObject[] button;
     // Use this for initialization
     void Start () {
         trans = transform;
@@ -21,7 +23,15 @@ public class enemyBullet : MonoBehaviour {
         if (col.gameObject.tag == "Player")
         {
             Destroy(col.gameObject);
+            gameOver();
         }
 
+    }
+
+    public void gameOver()
+    {
+        button = GameObject.FindGameObjectsWithTag("gobtn");
+		for(int i = 0; i< button.Length;i++)
+			button[i].transform.Translate(new Vector3(0, 300 , 0));
     }
 }
---
+++ GAM-DEV Space Invaders/Assets/GameOver.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class GameOver : MonoBehaviour {
+	public Button button;
+	public Transform trans;
+	// Use this for initialization
+	void Start () {
+		trans = transform;
+		//button = Button;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void gameOver(){
+		//button = (Button) GameObject.FindGameObjectWithTag ("gobtn");
+		//button.transform.Translate(new Vector3(0,59,0));
+    }
+}
